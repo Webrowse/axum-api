b542d2d6ee34d1dbb711a30801e6b0eef60bb6a7
Webrowse <great.adarsh@gmail.com>
2025-11-30 02:05:09 +0530

    loading port from .env file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-// use axum::Router;
 
 mod routes;
 mod config;
 
 #[tokio::main]
 async fn main(){
+
+    let config = config::Config::from_env();
+
     let app = routes::routes();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(config.addr()).await.unwrap();
 
+    println!("Server is running on {}", config.addr());
+    
     axum::serve(listener, app).await.unwrap();
 }
 
