5a2291be7e533a04a837c17a6c4a9f5aeeb00f7a
Webrowse <great.adarsh@gmail.com>
2025-12-01 19:08:34 +0530

    add state.rs, connected db with state

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-
-mod routes;
 mod config;
+mod routes;
+mod state;
+
+use axum::Router;
+use sqlx::PgPool;
 
 #[tokio::main]
-async fn main(){
-
+async fn main() {
     let config = config::Config::from_env();
 
-    let app = routes::routes();
+    let db = PgPool::connect(&std::env::var("DATABASE_URL").unwrap())
+        .await
+        .expect("Error connecting DB");
+
+    let state = state::AppState { db };
+    let app = routes::routes().with_state(state);
 
     let listener = tokio::net::TcpListener::bind(config.addr()).await.unwrap();
 
     println!("Server is running on {}", config.addr());
-    
+
     axum::serve(listener, app).await.unwrap();
 }
-
